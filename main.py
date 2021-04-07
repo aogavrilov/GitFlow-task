4f16a1ce993b30c97da26a2a11dde8ea9ab8f39d
Alexey <alexgavrilov28@gmail.com>
2021-04-07 14:21:55 +0300

    Added get_name, get_age to Member

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ class Member:
         self.name = name
         self.age = age
 
+    def get_age(self) -> int:
+        return self.age
+
+    def get_name(self) -> str:
+        return self.name
+
 
 member = Member("Test", 19)
