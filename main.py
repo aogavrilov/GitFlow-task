73725438cddd8857fb3d67f575a389f832f2a0e6
Alexey <alexgavrilov28@gmail.com>
2021-04-07 14:27:30 +0300

    Added Work class

--- main.py
+++ main.py
@@ -12,5 +12,23 @@ class Member:
     def get_name(self) -> str:
         return self.name
 
+class Work:
+    name: str
+    salary: int
+    responsibilities: str
+
+    def __init__(self, name: str, salary: int, responsibilities: str) -> None:
+        self.name = name
+        self.salary = salary
+        self.responsibilities = responsibilities
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_salary(self) -> int:
+        return self.salary
+
+    def get_responsibilities(self) -> str:
+        return self.responsibilities
 
 member = Member("Test", 19)
